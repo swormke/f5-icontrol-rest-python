ec513ebacb60179450c10f579ae9bbcf29658afa
Shawn Wormke <shawn@f5.com>
2016-01-14 15:37:31 -0700

    SQUASH: test

--- docs/conf.py
+++ docs/conf.py
@@ -32,21 +32,24 @@ from recommonmark.parser import CommonMarkParser
 # ones.
 extensions = [
     'sphinx.ext.autodoc',
-    'sphinx.ext.doctest',
-    'sphinx.ext.viewcode',
 ]
+
+#    'sphinx.ext.doctest',
+#    'sphinx.ext.viewcode',
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
+'''
 source_parsers = {
     '.md': CommonMarkParser
 }
+'''
 
-# source_suffix = '.rst'
-source_suffix = ['.rst', '.md']
+source_suffix = '.rst'
+#source_suffix = ['.rst', '.md']
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
